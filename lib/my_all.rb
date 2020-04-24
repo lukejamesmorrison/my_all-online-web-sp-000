d5016cc2d25b774625b24bf15e7cc6f3b4df1eee
Luke Morrison <lukejamesmorrison@gmail.com>
2020-04-24 13:31:41 +0000

    Automatically backed up by Learn

--- lib/my_all.rb
+++ lib/my_all.rb
@@ -5,12 +5,15 @@ require 'pry'
 def my_all?(collection)
 
   i = 0
+  return_values = []
   
   while i < collection.length do
     
-    yield(collection[i])
+    return_values << yield(collection[i])
     i += 1
     
   end
+  
+  return_values
 
 end
